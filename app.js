57e0bb9ad7283d1f06471e1641ce4269d76475f2
yamaguchi-yoshiki <102662423+yamaguchi-yoshiki@users.noreply.github.com>
2022-05-04 11:51:08 +0900

    [Add] JS-2

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-alert('Hello World');
+let hello = "Hello World";
+let str1 = "Hello";
+let str2 = "World!!";
+
+let orange = 100;
+let apple = 120;
+
+let max = 100;
+let num = 1;
+let number = 0;
+let count = 0;
+
+//alert(hello);
+//alert(str1 + str2);
+
+//if(orange < apple){
+//  alert("みかんの値段がりんごより安い");
+//}else if(orange == apple){
+//  alert("みかんとりんごが同じ値段");
+//}else{
+//  alert("みかんの値段がりんごより高い");
+//}
+
+//while(num < max){
+//  num = num * 2;
+//  count += 1;
+//}
+//alert("2を掛け続けて" + max + "を超えるのに必要だった回数は" + count + "回です");
+
+for(i=1; i<11; i++){
+  number += i;
+}
+alert("1から10まで足し算した結果は"+number+"です");
+
+// 足し算
+alert(4 + 3);
+// 引き算
+alert(8 - 5);
+// 掛け算
+alert(2*6);
+// 割り算
+alert(10/2);
---
+++ practice-variable/sample.js
@@ -0,0 +1,18 @@
+let nickname = "taro"
+console.log(nickname)
+nickname = "jiro"
+console.log(nickname)
+
+var str = "webcamp"
+
+function foo() {
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+
+foo()
+
+for (let i = 1; i < 10; i++) {
+  console.log(i)
+}
